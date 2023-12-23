cbdb014cd3c89a7d3c854c3152aeedc865062ba2
David Irwin <david.irwin@stormharvester.com>
2023-12-23 13:22:59 +0000

    Day 10 puzzle 2

--- Day 10/p2.py
+++ Day 10/p2.py
@@ -81,52 +81,18 @@ def trace(tile_dict, start_tile, from_direction=None):
 
     return trace_end, next_tile, direction
 
-def check_for_channels(tile_dict):
-    channels_no = 0
-    for location, tile in tile_dict.items():
-        try:
-            if tile.name == '7':
-                if tile.east_tile.name in ['F', '|']:
-                    tile.channel_entry = True
-                    tile.east_tile.channel_entry = True
-                    channels_no += 1
-                if tile.north_tile.name in ['J', '-']:
-                    tile.channel_entry = True
-                    tile.north_tile.channel_entry = True
-                    channels_no += 1
-            if tile.name == 'J':
-                if tile.east_tile.name in ['L', '|']:
-                    tile.channel_entry = True
-                    tile.east_tile.channel_entry = True
-                    channels_no += 1
-                if tile.south_tile.name in ['7', '-']:
-                    tile.channel_entry = True
-                    tile.south_tile.channel_entry = True
-                    channels_no += 1
-            if tile.name == 'L':
-                if tile.west_tile.name in ['J', '|']:
-                    tile.channel_entry = True
-                    tile.west_tile.channel_entry = True
-                    channels_no += 1
-                if tile.south_tile.name in ['F', '-']:
-                    tile.channel_entry = True
-                    tile.south_tile.channel_entry = True
-                    channels_no += 1
-            if tile.name == 'F':
-                if tile.west_tile.name in ['7', '|']:
-                    tile.channel_entry = True
-                    tile.west_tile.channel_entry = True
-                    channels_no += 1
-                if tile.north_tile.name in ['L', '-']:
-                    tile.channel_entry = True
-                    tile.north_tile.channel_entry = True
-                    channels_no += 1
-        except AttributeError as e:
-            print(e)
 
-    print(f'Channels: {channels_no}')
+def count_internal_tiles(tile_dict):
+    total = 0
+    for tile in tile_dict.values():
+        if tile.name == 'I':
+            total += 1
 
-def print_dict(dict, max_x, max_y):     # for debugging
+    return total
+
+
+def print_dict(dict, max_x, max_y):
+    print('\n\n')
     for y in range(max_y):
         line = ''
         for x in range(max_x):
@@ -166,13 +132,11 @@ start_tile.get_starting_directions()
 # iterate through loop from starting tile
 next_tile = start_tile
 direction = None
-# steps = 0
 ret_tup = (False, next_tile, direction)
 while ret_tup[0] is False:
     ret_tup = trace(tile_dict, next_tile, direction)
     next_tile = ret_tup[1]
     direction = ret_tup[2]
-    # steps += 1
 
 # print_dict(tile_dict, max_x, max_y)
 
@@ -183,56 +147,50 @@ for location, tile in tile_dict.items():
 
 print_dict(tile_dict, max_x, max_y)
 
-# search for channels between pipes
-check_for_channels(tile_dict)
-
-# change tiles outside loop to 0
-x = 0
-y = 0
-current_tile = tile_dict[(x, y)]
-if current_tile.name != '.':
-    print("Starting point for changing tiles to 0 isn't '.'. Pick a different starting location.")
-
-to_be_tested = [(x, y)]
-tiles_tested = []
-while len(to_be_tested) > 0:
-    current_location = to_be_tested.pop()
-    tiles_tested.append(current_location)
-    current_tile = tile_dict[current_location]
-    current_tile.outside_loop = True
-    try:
-        if current_tile.north_tile.name == '.':
-            current_tile.north_tile.outside_loop = True
-            current_tile.north_tile.name = '0'
-            to_be_tested.append((current_tile.north_tile.location))
-    except AttributeError:
-        pass
-    try:
-        if current_tile.east_tile.name == '.':
-            current_tile.east_tile.outside_loop = True
-            current_tile.east_tile.name = '0'
-            to_be_tested.append((current_tile.east_tile.location))
-    except AttributeError:
-        pass
-    try:
-        if current_tile.south_tile.name == '.':
-            current_tile.south_tile.outside_loop = True
-            current_tile.south_tile.name = '0'
-            to_be_tested.append((current_tile.south_tile.location))
-    except AttributeError:
-        pass
-    try:
-        if current_tile.west_tile.name == '.':
-            current_tile.west_tile.outside_loop = True
-            current_tile.west_tile.name = '0'
-            to_be_tested.append((current_tile.west_tile.location))
-    except AttributeError:
-        pass
+# check if . characters are inside or outside loop
+for y in range(max_y + 1):
+    outside = True
+    top_edge = False
+    bottom_edge = False
+    for x in range(max_x + 1):
+        if tile_dict[(x, y)].name == '.':
+            if outside:
+                tile_dict[(x, y)].name = 'O'
+            else:
+                tile_dict[(x, y)].name = 'I'
+        if tile_dict[(x, y)].name == '|':
+            if outside:
+                outside = False
+            else:
+                outside = True
+        if tile_dict[(x, y)].name == 'F':
+            top_edge = True
+        if tile_dict[(x, y)].name == 'L':
+            bottom_edge = True
+        if tile_dict[(x, y)].name == '7':
+            if top_edge:
+                top_edge = False
+            if bottom_edge:
+                bottom_edge = False
+                if outside:
+                    outside = False
+                else:
+                    outside = True
+        if tile_dict[(x, y)].name == 'J':
+            if top_edge:
+                top_edge = False
+                if outside:
+                    outside = False
+                else:
+                    outside = True
+            if bottom_edge:
+                bottom_edge = False
 
 print_dict(tile_dict, max_x, max_y)
 
+total = count_internal_tiles(tile_dict)
 
-# print(f'Steps: {steps}')
-# print(f'Steps to middle point: {int(steps/2)}')
+
+print(f'Total internal tiles: {total}')
 print('Process complete')
 print(f"Process time: {round(time() - start_time, 2)} seconds.")
